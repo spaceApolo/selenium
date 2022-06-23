c8d1870dc119d52aa5f2f4837a53ee8660e0659c
cesar <cesarumeresdev@gmail.com>
2022-06-23 12:43:20 -0500

    añadiendo searchtest donde vemos diferentes ejemplos de como verificar diversos campos css, vemos los diversos metodos para encontrar la informacion usando diferentes tipos de llamadas

--- hello_world.py
+++ hello_world.py
@@ -1,26 +1,26 @@
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 
 class HelloWorld(unittest.TestCase):
+    
+    __driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
+   #__driver = webdriver.Chrome(service = __service)
 
-    def setUp(self) -> None:
-        self.driver = webdriver.Chrome(executable_path ='./drivers/chromedriver')
-        #browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
-        driver = self.driver
-        driver.implicity_wait(10)
+    @classmethod
+    def setUpClass(self):
+        self.__driver.implicitly_wait(10)
 
     def test_hello_world(self):
-        driver = self.driver
-        driver.get('https://www.platzi.com')
+        self.__driver.get("https://www.platzi.com")
     
     def test_visit_wikipedia(self):
-        self.driver.get('www.google.com')
+        self.__driver.get("https://www.wikipedia.org/")
 
+    @classmethod
+    def tearDownClass(self):
+        self.__driver.quit()
 
-    def tearDown(self) -> None:
-        self.driver.quit() 
-
-    if __name__ == '__main__':
-        unittest.main(verbosity=2, testRunner= HTMLTestRunner(output = 'reportes', report_name = 'hello-world-report'))
+if __name__ == '__main__':
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='reportes', report_name='hello_world_report'))
--- searchtest.py
+++ searchtest.py
@@ -14,20 +14,40 @@ class HomePageTest(unittest.TestCase):
         
         
         self.driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
-        #self.__driver.get('https://www.google.com')
         driver = self.driver
         driver.get('http://demo-store.seleniumacademy.com/')
         driver.maximize_window()
-
+        driver.implicitly_wait(15)
 
 
     def test_search_text_field(self):
-        search_field = self.driver.find_element(By.ID,'search')
-        #self.driver.get("https://www.google.com")
+        search_field = self.driver.find_element_by_id("search")
+
+    def test_search_text_field_by_name(self):
+        search_field = self.driver.find_element_by_name('q')
+
+    def test_search_text_field_by_class_name(self):
+        search_field = self.driver.find_element_by_class_name('input-text')
+    
+    def test_search_button_enabled(self):
+        search_field = self.driver.find_element_by_class_name('button')
+    
+    def test_count_of_promo_banners_images(self):
+        banner_list = self.driver.find_element_by_class_name('promos')
+        banners = banner_list.find_elements_by_tag_name('img')
+        self.assertEqual(3, len(banners))
+
+    def test_vip_promo(self):
+        vip_promo = self.driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div/div[2]/div[1]/ul/li[3]/a/img')
+
+    
+    def test_shopping_cart(self):
+        shopping_cart_icon = self.driver.find_element_by_css_selector('div.header-minicart span.icon')
+
 
     @classmethod
     def tearDownClass(self):
-        self.__driver.quit()
+        self.driver.quit()
     
 if __name__ == '__main__':
     unittest.main(verbosity=2, testRunner=HTMLTestRunner(output='reportes', report_name='hello_world_report'))
